be81fcdb1c54862a13d8eedc785c9c70ceb4d0f4
Alex WERNER <alex@giraf.world>
2017-01-30 12:48:18 +0100

    [Feat] Added regex hasUri

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
-# 1.2.0 - 30/01/2017
+# 1.2.1 - 30/01/2017
+* Added Regex for parsing a string that contains a valid URI
+  * `regex.hasUri("t.ly/12bl3xX");` will return `true`
+  * `regex.hasUri("Check it out t.ly/12bl3xX");` will return `true`
 
-* Added Regex for valid URI.
+# 1.2.0 - 30/01/2017
+* Added Regex parsing a string and checks if it's a valid URI.
+  * `regex.isUri("t.ly/12bl3xX");` will return `true`
+  * `regex.isUri("Check it out t.ly/12bl3xX");` will return `false`
--- test/khal-test.js
+++ test/khal-test.js
@@ -315,6 +315,8 @@ describe('- Khal utils ', function () {
         it('should verify a valid uri', function(){
           regex.isUri("ftp://128.125.132.10").should.equal(true);
           regex.isUri("http://google.fr").should.equal(true);
+          regex.isUri("Hi google.fr").should.equal(false);
+          regex.isUri("Hi google.fr how are u?").should.equal(false);
           regex.isUri("HTTP://GOOGLE.FR").should.equal(true);
           regex.isUri("www.google.fr").should.equal(true);
           regex.isUri("google.fr").should.equal(true);
@@ -332,21 +334,46 @@ describe('- Khal utils ', function () {
           regex.isUri("toto du 4758").should.equal(false);
           regex.isUri("Hey suis moi sur mon profil giraf ;)").should.equal(false);
 
-            var text = `^ asserts position at start of a line
-              Non-capturing group (?:(?:https?|ftp):\/\/)?
-              ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
-              Non-capturing group (?:https?|ftp)
-              1st Alternative https?
-              http matches the characters http literally (case insensitive)
-              s? matches the character s literally (case insensitive)
-              ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
-              2nd Alternative ftp
-              ftp matches the characters ftp literally (case insensitive)
-              : matches the character : literally (case insensitive)
-              \/ matches the character / literally (case insensitive)
-              \/ matches the character / literally (case insensitive)`;
-          regex.isUri(text).should.equal(false);
 
+        });
+        it('should verify a text contains a uri', function(){
+
+                      var text = `^ asserts position at start of a line
+                        Non-capturing group (?:(?:https?|ftp):\/\/)?
+                        ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
+                        Non-capturing group (?:https?|ftp)
+                        1st Alternative https?
+                        http matches the characters http literally (case insensitive)
+                        s? matches the character s literally (case insensitive)
+                        ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
+                        2nd Alternative ftp
+                        ftp matches the characters ftp literally (case insensitive)
+                        : matches the character : literally (case insensitive)
+                        \/ matches the character / literally (case insensitive)
+                        \/ matches the character / literally (case insensitive)`;
+                        regex.hasUri(text).should.equal(false);
+                        text+=" but now we have a URI like www.giraf.world or stuff like this";
+                        regex.hasUri(text).should.equal(true);
+                        regex.hasUri("ftp://128.125.132.10").should.equal(true);
+                        regex.hasUri("http://google.fr").should.equal(true);
+                        regex.hasUri("Hi google.fr").should.equal(true);
+                        regex.hasUri("Hi google.fr how are u?").should.equal(true);
+                        regex.hasUri("HTTP://GOOGLE.FR").should.equal(true);
+                        regex.hasUri("www.google.fr").should.equal(true);
+                        regex.hasUri("google.fr").should.equal(true);
+                        regex.hasUri("go.fr").should.equal(true);
+                        regex.hasUri("http://www.google.fr").should.equal(true);
+                        regex.hasUri("http://google.fr").should.equal(true);
+                        regex.hasUri("https://www.google.fr").should.equal(true);
+                        regex.hasUri("https://google.fr").should.equal(true);
+                        regex.hasUri("gogl.com").should.equal(true);
+                        regex.hasUri("google.clo").should.equal(true);
+                        regex.hasUri("www.gogrgrgro.com.sd").should.equal(true);
+                        regex.hasUri("bit.ly/12bl3xX").should.equal(true);
+                        regex.hasUri("t.ly/12bl3xX").should.equal(true);
+                        regex.hasUri("www.google.co.in").should.equal(true);
+                        regex.hasUri("toto du 4758").should.equal(false);
+                        regex.hasUri("Hey suis moi sur mon profil giraf ;)").should.equal(false);
         });
     })
     describe('Sorting',function(){
